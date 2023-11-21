79226250d654f19093472af0e1472a629a7cd023
Tuomas Åfelt <tuomas.afelt@gmail.com>
2023-11-21 20:11:36 +0200

    Pass environment variable to database container.

--- Database/entrypoint.sh
+++ Database/entrypoint.sh
@@ -4,15 +4,13 @@ export DATA_SOURCE="mssql"
 export APP_DATABASE="appdb"
 export APP_TEST_DATABASE="${APP_DATABASE}_test"
 export USER_ID="sa"
-export PASSWORD="<YourStrong!Passw0rd>"
+export PASSWORD=$MSSQL_SA_PASSWORD
 
 export SYSTEM_CONNECTION_STRING="Data Source=${DATA_SOURCE};User ID=${USER_ID};Password=${PASSWORD};"
 export APP_CONNECTION_STRING="Data Source=${DATA_SOURCE};Database=${APP_DATABASE};User ID=${USER_ID};Password=${PASSWORD};"
 export APP_TEST_CONNECTION_STRING="Data Source=${DATA_SOURCE};Database=${APP_TEST_DATABASE};User ID=${USER_ID};Password=${PASSWORD};"
 
 dotnet restore
-
-dotnet clean /usr/src/Database/Database.sln --configuration Release
 
 dotnet build /usr/src/Database/Database.sln --configuration Release --no-incremental
 
