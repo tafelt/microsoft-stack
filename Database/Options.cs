01751b550a2fc51ac5a72d794b8d0d932b6cab99
Tuomas Åfelt <tuomas.afelt@gmail.com>
2023-11-25 15:23:36 +0200

    - Database: Added help texts for arguments. - Server: Removed https redirection.

--- Database/Options.cs
+++ Database/Options.cs
@@ -4,12 +4,12 @@ namespace Database;
 
 public class Options
 {
-  [Option("systemConnectionString", Required = true)]
+  [Option("systemConnectionString", Required = true, HelpText = "Connection string used to access system databases.")]
   public string SystemConnectionString { get; set; }
 
-  [Option("baseConnectionString", Required = true)]
+  [Option("baseConnectionString", Required = true, HelpText = "Connection string used to access application database.")]
   public string BaseConnectionString { get; set; }
 
-  [Option("database", Required = true)]
+  [Option("database", Required = true, HelpText = "Database name.")]
   public string Database { get; set; }
 }
