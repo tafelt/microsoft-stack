132ec24839851d8aea295b79833923f642daf776
Tuomas Åfelt <tuomas.afelt@gmail.com>
2023-11-26 17:04:24 +0200

    Create sql connection using environment variable.

--- Database/entrypoint.sh
+++ Database/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DATA_SOURCE="mssql"
-export APP_DATABASE="appdb"
+export DATA_SOURCE=$MSSQL_DATA_SOURCE
+export APP_DATABASE=$MSSQL_APP_DATABASE
 export APP_TEST_DATABASE="${APP_DATABASE}_test"
-export USER_ID="sa"
+export USER_ID=$MSSQL_USER_ID
 export PASSWORD=$MSSQL_SA_PASSWORD
 
 export SYSTEM_CONNECTION_STRING="Data Source=${DATA_SOURCE};User ID=${USER_ID};Password=${PASSWORD};"
--- Server/Infrastructure/Repositories/SqlConnectionFactory.cs
+++ Server/Infrastructure/Repositories/SqlConnectionFactory.cs
@@ -6,21 +6,10 @@ namespace Infrastructure.Repositories;
 
 internal sealed class SqlConnectionFactory : ISqlConnectionFactory
 {
-  private readonly IConfiguration _configuration;
-
-  public SqlConnectionFactory(IConfiguration configuration)
-  {
-    _configuration = configuration;
-  }
-
   public SqlConnection CreateConnection()
   {
-    // TODO: Get connection string from environment variables
     return new SqlConnection(
-      "Data Source=mssql;Database=appdb;User ID=sa;Password=<YourStrong!Passw0rd>;Trusted_Connection=false;TrustServerCertificate=true;"
-    // _configuration.GetConnectionString(
-    //   "DOTNET_APP_DATABASE_CONNECTION_STRING"
-    // )
+      Environment.GetEnvironmentVariable("DOTNET_DEFAULT_CONNECTION_STRING")
     );
   }
 }
