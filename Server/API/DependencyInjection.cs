4b762e9dbdaff171d80b86c42cc52f6243ec9eeb
Tuomas Åfelt <tuomas.afelt@gmail.com>
2024-02-04 13:29:24 +0200

    Added separate dependency injection for middlewares.

--- Server/API/DependencyInjection.cs
+++ Server/API/DependencyInjection.cs
@@ -8,18 +8,17 @@ public static class DependencyInjection
 {
   public static IServiceCollection AddPresentation(this IServiceCollection services)
   {
-    services.AddTransient<GlobalExceptionHandlingMiddleware>();
+    services.AddCors();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    services.AddMiddlewares();
+    services.AddLogging();
+    services.AddMapping();
 
     services.AddCarter(configurator: c =>
     {
       c.WithValidatorLifetime(ServiceLifetime.Scoped);
     });
-
-    services.AddCors();
-    services.AddEndpointsApiExplorer();
-    services.AddSwaggerGen();
-    services.AddLogging();
-    services.AddMapping();
 
     services.Configure<RouteOptions>(options =>
     {
---
+++ Server/API/Middlewares/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Middlewares;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddMiddlewares(this IServiceCollection services)
+  {
+    services.AddTransient<GlobalExceptionHandlingMiddleware>();
+
+    return services;
+  }
+}
